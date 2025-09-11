14d8752df42d876217e48fd75a40b64f90e497f5
Harry Askham <harryaskham@gmail.com>
2025-09-11 14:18:33 +0100

    working lambdas

---
+++ lambdas/cat.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Identity lambda - passes audio through unchanged
+# This is the simplest possible lambda that demonstrates the interface
+exec cat
--- lambdas/identity.sh
+++ lambdas/identity.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 # Identity lambda - passes audio through unchanged
-# This is the simplest possible lambda that demonstrates the interface
-exec cat
+pacat -r --latency-msec=1 | pacat -p --latency-msec=1
