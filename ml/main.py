b60c71166fc7d60223aaf143accae74b239c03df
Harry Askham <harryaskham@gmail.com>
2025-09-13 22:44:25 +0100

    [rapid commit; squash later]

--- ml/main.py
+++ ml/main.py
@@ -3,8 +3,13 @@ from hs_tasnet import HSTasNet
 
 model = HSTasNet()
 
+
 audio = torch.randn(1, 2, 204800) # ~5 seconds of stereo
 
 separated_audios, _ = model(audio)
 
-assert separated_audios.shape == (1, 4, 2, 204800) # second dimension is the separated tracks
+assert separated_audios.shape == (1, 4, 2, 204800)
+drums = separated_audios[:, 0, :, :]
+bass = separated_audios[:, 1, :, :]
+vocals = separated_audios[:, 2, :, :]
+other = separated_audios[:, 3, :, :]
