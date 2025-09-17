9d1c69ad4c2e131e40c4cfd093bfbc73a15e2f55
Harry Askham <harryaskham@gmail.com>
2025-09-17 13:40:05 +0100

    [rapid commit; squash later]

--- ml/flake.nix
+++ ml/flake.nix
@@ -8,19 +8,6 @@
       url = "github:nix-community/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    uv2nix = {
-      url = "github:pyproject-nix/uv2nix";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    pyproject-build-systems = {
-      url = "github:pyproject-nix/build-system-pkgs";
-      inputs.pyproject-nix.follows = "pyproject-nix";
-      inputs.uv2nix.follows = "uv2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
@@ -28,9 +15,7 @@
       self,
       nixpkgs,
       flake-utils,
-      uv2nix,
       pyproject-nix,
-      pyproject-build-systems,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (system:
